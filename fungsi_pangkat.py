7487a43eed13276b0d93c35c7d15d54704237e5f
riskiindrawan27 <49383932+riskiindrawan27@users.noreply.github.com>
2019-05-03 14:00:45 +0700

    Delete fungsi_pangkat.py

--- fungsi_pangkat.py
+++
@@ -1,9 +0,0 @@
-def eksponensial(angka, pangkat):
-    hasil = 1
-    for i in range(pangkat):
-        hasil = hasil * angka
-    return hasil
-
-print(eksponensial(2,2))
-print(eksponensial(3,3))
-print(eksponensial(10,5))
